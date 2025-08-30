031afa5d8777b7255efe628826ec22223affbd66
divOmar <oa05058@gmail.com>
2025-08-30 10:20:08 +0300

    rm multer2

--- src/Middleware/mullter.middleware.js
+++ src/Middleware/mullter.middleware.js
@@ -1,32 +1,24 @@
 import multer from "multer";
 import fs from 'fs'
 
-// export const uploadMiddlewareHost =(desetinationPath='general',allawedExtention=[])=>{
-//     const destinationFolder =`Assets/${desetinationPath}`
-//     if (!fs.existsSync(destinationFolder)){
-//         fs.mkdirSync(destinationFolder)
-//     }
-//     const storage= multer.diskStorage({
-//         destination:function(req,file,cb){
-//             cb(null,destinationFolder)
-//         },
-//         filename:function(req,file,cb){
-//             const uniqesuffix=Date.now()+'-'+Math.round(Math.random()*1E9)
-//             cb(null,uniqesuffix+'_'+file.originalname)
-//         }
-//     })
+export const uploadMiddlewareHost =(desetinationPath='general',allawedExtention=[])=>{
+    // const destinationFolder =`Assets/${desetinationPath}`
+    // if (!fs.existsSync(destinationFolder)){
+    //     fs.mkdirSync(destinationFolder)
+    // }
+    const storage= multer.diskStorage({})
 
-//     const fileFilter=(req,file,cb)=>{
-//             if (allawedExtention.includes(file.mimetype)){
-//                 cb(null,true)
-//             }
-//             else{
-//                 cb(new Error("invailed file type"),false)
-//             }
-//         }
-//     const upload=multer({fileFilter,storage})
-//     return upload
-// }
+    const fileFilter=(req,file,cb)=>{
+            if (allawedExtention.includes(file.mimetype)){
+                cb(null,true)
+            }
+            else{
+                cb(new Error("invailed file type"),false)
+            }
+        }
+    const upload=multer({fileFilter,storage})
+    return upload
+}
 export const uploadCloud=(allawedExtention=[])=>{
    
     const storage= multer.diskStorage({})
